7567bf0f655500538ff7a71eb7edd424d628422c
BelyaevSergey <sergey.a.belyaev@gmail.com>
2020-03-17 22:37:36 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+   if (!array||array.length==0) {
+       return 0;
+   }
+   else {
+       function plusmin (a,b){
+          return a-b;
+        }
+       let minim= array.sort(plusmin).shift();
+       return minim;
+   }
+    }
+    
+ 
+
 
 exports.max = function max (array) {
-  return 0;
+    if (!array||array.length==0) {
+        return 0;
+    }
+    else {
+        function plusmin (a,b){
+           return b-a;
+         }
+        let maxim= array.sort(plusmin).shift();
+        return maxim;
+}
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    let sum=0;
+    if (!array||array.length==0) {
+        return 0;
+    }
+    else {
+        for (let key of array) 
+        {
+            sum=sum+key;
+            
+        }
+        let mid = sum/array.length;
+        return mid;
+    }
+
 }
